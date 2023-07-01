64452b1398948c1e417d3eff22d67934f92513c3
Priyanshu farkare <priyanshufarkare1443@gmail.com>
2023-07-01 14:56:35 +0530

    change px to rem

--- src/Components/Crew.js
+++ src/Components/Crew.js
@@ -5,6 +5,7 @@ import c1 from "../assets/crew/image-douglas-hurley.png";
 import c2 from "../assets/crew/image-mark-shuttleworth.png";
 import c3 from "../assets/crew/image-victor-glover.png";
 import c4 from "../assets/crew/image-anousheh-ansari.png";
+import "../assets/CSS/Crew.css";
 
 export default function Crew() {
   const [crew, setcrew] = useState(0)
@@ -12,16 +13,16 @@ export default function Crew() {
   const imgages = [c1, c2, c3, c4]
 
   return (
-    <div style={{ color: "white" }}>
-      <div>
+    <div id="crew">
+      <div id="crew_details">
         <div>
           <span>02</span>
-          <p>Meet your crew</p>
+          <span>Meet your crew</span>
         </div>
         <div>
-          <p>{data.crew[crew].role}</p>
-          <p>{data.crew[crew].name}</p>
-          <p>{data.crew[crew].bio}</p>
+          <p id="crew_role">{data.crew[crew].role}</p>
+          <p id="crew_name">{data.crew[crew].name}</p>
+          <p id="crew_bio">{data.crew[crew].bio}</p>
         </div>
         <div>
           <div onClick={() => { setcrew(0) }}>1</div>
@@ -30,8 +31,8 @@ export default function Crew() {
           <div onClick={() => { setcrew(3) }}>4</div>
         </div>
       </div>
-      <div>
-        <img src={imgages[crew]} alt="" />
+      <div id="crew_img">
+        {/* <img src={imgages[crew]} alt="" /> */}
       </div>
     </div>
   )
