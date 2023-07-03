c702619f14660febd2e7c1255a901a4ff4ec8400
Priyanshu farkare <priyanshufarkare1443@gmail.com>
2023-07-03 18:59:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 import "./assets/CSS/Homepage.css";
-import homedesktopbackground from "./assets/home/background-home-desktop.jpg";
-import destinationdesktopbackground from "./assets/destination/background-destination-desktop.jpg";
-import crewdesktopbackground from "./assets/crew/background-crew-desktop.jpg";
-import technologydesktopbackground from "./assets/technology/background-technology-desktop.jpg";
 
+// desktop img import 
+import homedb from "./assets/home/background-home-desktop.jpg";
+import destinationdb from "./assets/destination/background-destination-desktop.jpg";
+import crewdb from "./assets/crew/background-crew-desktop.jpg";
+import technologydb from "./assets/technology/background-technology-desktop.jpg";
+
+// tablet img import 
+import hometb from "./assets/home/background-home-tablet.jpg";
+import destinationtb from "./assets/destination/background-destination-tablet.jpg";
+import crewtb from "./assets/crew/background-crew-tablet.jpg";
+import technologytb from "./assets/technology/background-technology-tablet.jpg";
+
+// components import 
 import Navigation from "./Components/Navigation";
 import Homepagedata from "./Components/Homepagedata"
 import Destination from "./Components/Destination";
@@ -17,14 +26,20 @@ import Technology from "./Components/Technology";
 function App() {
   const [changedata, setchangedata] = useState(0);
 
+  const desktopbackground = [homedb, destinationdb, crewdb, technologydb]
+  const tabletbackground = [hometb, destinationtb, crewtb, technologytb]
+
   const pathchange = () => {
     var pathname = window.location.pathname;
     pathname === "/" ? setchangedata(0) : pathname === "/destination" ? setchangedata(1) : pathname === "/crew" ? setchangedata(2) : setchangedata(3)
   }
 
   const backgroundimg = {
-    backgroundImage: `url(${changedata === 0 ? homedesktopbackground : changedata === 1 ? destinationdesktopbackground : changedata === 2 ? crewdesktopbackground : technologydesktopbackground})`
+    backgroundImage: `url(${window.innerWidth >= 800 ? desktopbackground[changedata] : tabletbackground[changedata]
+      })`
   }
+
+
 
   return (
     <div id="homepage" style={backgroundimg} >
